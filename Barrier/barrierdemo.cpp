44e2e09426d9a0e21ecbd577d36c3e3f2127e3e4
Israel Ben David <israelbd@cs.huji.ac.il>
2023-05-10 19:31:33 +0300

    thread problems

--- Barrier/barrierdemo.cpp
+++ Barrier/barrierdemo.cpp
@@ -2,49 +2,55 @@
 #include <pthread.h>
 #include <cstdio>
 
-#define MT_LEVEL 5
+#define MT_LEVEL 50
 
 struct ThreadContext {
-	int threadID;
-	Barrier* barrier;
+    int threadID;
+    Barrier *barrier;
 };
 
-
-void* foo(void* arg)
+void *foo (void *arg)
 {
-	ThreadContext* tc = (ThreadContext*) arg;
-	printf("Before barriers: %d\n", tc->threadID);
+  ThreadContext *tc = (ThreadContext *) arg;
+  printf ("Before barriers: %d\n", tc->threadID);
+  int c = 0;
+  for (int i = 0; i < 1000000000; ++i)
+    {
+      c++;
+    }
 
-	tc->barrier->barrier();
+  tc->barrier->barrier ();
 
-	printf("Between barriers: %d\n", tc->threadID);
+  printf ("Between barriers: %d\n", tc->threadID);
 
-	tc->barrier->barrier();
+  tc->barrier->barrier ();
 
-	printf("After barriers: %d\n", tc->threadID);
+  printf ("After barriers: %d\n", tc->threadID);
 
-	return 0;
+  return 0;
 }
 
-
-int main(int argc, char** argv)
+int main (int argc, char **argv)
 {
-	pthread_t threads[MT_LEVEL];
-	ThreadContext contexts[MT_LEVEL];
-	Barrier barrier(MT_LEVEL);
+  pthread_t threads[MT_LEVEL];
+  ThreadContext contexts[MT_LEVEL];
+  Barrier barrier (MT_LEVEL);
 
-	for (int i = 0; i < MT_LEVEL; ++i) {
-		contexts[i] = {i, &barrier};
-	}
+  for (int i = 0; i < MT_LEVEL; ++i)
+    {
+      contexts[i] = {i, &barrier};
+    }
 
+  for (int i = 0; i < MT_LEVEL; ++i)
+    {
+      pthread_create (threads + i, NULL, foo, contexts + i);
+    }
 
-	for (int i = 0; i < MT_LEVEL; ++i) {
-		pthread_create(threads + i, NULL, foo, contexts + i);
-	}
+//  for (int i = 0; i < MT_LEVEL; ++i)
+//    {
+//      pthread_join (threads[i], NULL);
+//    }
+  printf ("hiiiiiii\n");
 
-	for (int i = 0; i < MT_LEVEL; ++i) {
-		pthread_join(threads[i], NULL);
-	}
-
-	return 0;
+  return 0;
 }
--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -22,6 +22,12 @@ struct ThreadContext {
 /***** Global Variables *****/
 std::vector<IntermediateVec> intermediate_vecs;
 JobState job_state;
+OutputVec output_vec_p;
+InputVec input_vec_p;
+std::atomic<int> map_counter;
+std::atomic<int> reduce_counter;
+std::vector<IntermediateVec> shuffled_intermediate_vecs;
+
 
 /***** Internal Interface *****/
 
@@ -77,26 +83,27 @@ void emit3 (K3* key, V3* value, void* context){
   ((OutputVec *)context)->push_back ({key, value}); // todo: static cast?
 }
 
-
-void* worker(void* arg){
-
-  auto* thread_context = (ThreadContext*) arg;
-  /* Map Phase */
+/* In this phase each thread reads pairs of (k1, v1) from the input vector and calls the map function
+ * on each of them. The map function in turn will produce (k2, v2) and will call the emit2 function to
+ * update the framework databases.
+ */
+void map_phase(ThreadContext* thread_context){
   job_state.stage = MAP_STAGE;
-  std::atomic<int> counter(0);
-  for (auto pair: *(thread_context->inputVec))
-    {
-      // call map
-      IntermediateVec intermediate_vec;
-      thread_context->client->map (pair.first, pair.second, &intermediate_vec);
-      intermediate_vecs.push_back (intermediate_vec);
-//      counter++;
-//      job_state.percentage = (float)counter*100/(float)thread_context->inputVec->size();
-    }
-    job_state.percentage = 30;
-  // -- barrier -- ???
+  while (map_counter < thread_context->inputVec->size()){
+    int old_value = ++map_counter-1; // todo: is it ok?
+    IntermediateVec intermediate_vec;
+    InputPair pair = thread_context->inputVec->at(old_value);
+    // call map
+    thread_context->client->map (pair.first, pair.second, &intermediate_vec);
+    intermediate_vecs.push_back (intermediate_vec);
+  }
 
-  /* Sort Phase */
+  job_state.percentage = 30;
+}
+
+/*  each thread will sort its intermediate vector according to the keys within
+ */
+void sort_phase(ThreadContext* thread_context){
   for (auto intermediate_vec: intermediate_vecs)
     {
       std::sort(intermediate_vec.begin(), intermediate_vec.end(),
@@ -106,38 +113,64 @@ void* worker(void* arg){
     }
   job_state.percentage = 60;
 
-  // -- barrier -- !!
+}
 
-  /* Shuffle Phase */
+/* Thread number 0 will combine the vectors into a single data structure.
+ * The function uses an atomic counter for counting the number of vectors in it.
+ */
+void shuffle_phase(ThreadContext* thread_context){
   job_state.stage = SHUFFLE_STAGE;
-  std::vector<IntermediateVec> shuffled_intermediate_vecs;
   K2* max_key_element = max_key();
   while (max_key_element){
-    IntermediateVec pairs_vec;
+      IntermediateVec pairs_vec;
       for (auto& intermediate_vec: intermediate_vecs)
         {
           if (is_equal (intermediate_vec.back().first, max_key_element)){
               pairs_vec.push_back(intermediate_vec.back());
               intermediate_vec.pop_back();
-          }
+            }
         }
       shuffled_intermediate_vecs.push_back(pairs_vec);
       max_key_element = max_key ();
-
-  }
+    }
   job_state.percentage = 80;
+}
 
-
-  // -- barrier -- ?
-
-  /* Reduce Phase */
+/*
+ * the function pops a vector from the back of the queue and run reduce on it.
+ */
+void reduce_phase(ThreadContext* thread_context){
   job_state.stage = REDUCE_STAGE;
-  for (auto& shuffled_vec: shuffled_intermediate_vecs)
-    {
+  while (reduce_counter < shuffled_intermediate_vecs.size()){
+      int old_value = ++reduce_counter-1; // todo: is it ok?
+      IntermediateVec shuffled_vec = shuffled_intermediate_vecs.at(old_value);
       // call reduce
       thread_context->client->reduce(&shuffled_vec, thread_context->outputVec);
     }
   job_state.percentage = 100;
+}
+
+void* worker(void* arg){
+
+  auto* thread_context = (ThreadContext*) arg;
+  /* Map Phase */
+  map_phase (thread_context);
+
+  /* Sort Phase */
+  sort_phase (thread_context);
+  thread_context->barrier->barrier();
+
+  /* Shuffle Phase */
+  if (thread_context->threadID == 0){ // only thread 0
+    shuffle_phase (thread_context);
+  }
+
+
+  thread_context->barrier->barrier();
+
+  /* Reduce Phase */
+  reduce_phase (thread_context);
+
 }
 
 JobHandle startMapReduceJob(const MapReduceClient& client,
@@ -146,23 +179,33 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
 {
   // job init
   job_state = {UNDEFINED_STAGE, 0};
+  map_counter = 0;
+  reduce_counter = 0;
 
+
+  output_vec_p = outputVec;
+  input_vec_p = inputVec;
   pthread_t threads[multiThreadLevel];
   ThreadContext contexts[multiThreadLevel];
-  Barrier barrier(multiThreadLevel);
+
+//  Barrier barrier(multiThreadLevel);
+  auto* barrier = new Barrier(multiThreadLevel);
 
   for (int i = 0; i < multiThreadLevel; ++i) {
-      contexts[i] = {i, &barrier, &client, &inputVec, &outputVec};
+      contexts[i] = {i, barrier, &client, &input_vec_p, &output_vec_p};
     }
 
 
-  for (int i = 0; i < 1; ++i) {
+  for (int i = 0; i < multiThreadLevel; ++i) {
       pthread_create(threads + i, NULL, worker, contexts + i);
     }
+printf("it's not going to end today");
+  printf("go home");
+//  for (int i = 0; i < multiThreadLevel; ++i)
+//    {
+//      pthread_join (threads[i], NULL);
+//    }
 
-  for (int i = 0; i < 1; ++i) {
-      pthread_join(threads[i], NULL);
-    }
   return (JobHandle) &job_state;
 
 }
